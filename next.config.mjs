e6c27041191ce27735a0a63caaca68964eae16fe
ChenHao Chen <crazycjh@gmail.com>
2025-03-23 22:45:55 +0800

    fix: update next.config.mjs for api query

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`
-        // destination: "https://c105-13-115-215-106.ngrok-free.app/:path*"
+        destination: "https://c105-13-115-215-106.ngrok-free.app/:path*"
       },
     ];
   },
